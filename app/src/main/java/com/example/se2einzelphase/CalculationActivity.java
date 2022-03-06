2e0059c6ad5810d0233b91be457e953acdcb1b76
Kevin Ernst Kuttnig <94113871+kuttnig@users.noreply.github.com>
2022-03-06 20:49:41 +0100

    Add popup menu to switch activities
    This commit adds a menu. The menu opens, if the menubutton is clicked. With a click on a menuitem it is possible to start a new activity.

--- app/src/main/java/com/example/se2einzelphase/CalculationActivity.java
+++ app/src/main/java/com/example/se2einzelphase/CalculationActivity.java
@@ -1,14 +1,35 @@
 package com.example.se2einzelphase;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
-public class CalculationActivity extends AppCompatActivity {
+public class CalculationActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculation);
+    }
+
+    public void showPopup(View v) {
+        PopupMenu popupMenu = new PopupMenu(this, v);
+        popupMenu.inflate(R.menu.menu_items);
+        popupMenu.setOnMenuItemClickListener(this);
+        popupMenu.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        if (item.getItemId() == R.id.menu_item_network) {
+            Intent startNetworkIntent = new Intent(this, MainActivity.class);
+            startActivity(startNetworkIntent);
+            return true;
+        }
+        return false;
     }
 }
--- app/src/main/java/com/example/se2einzelphase/MainActivity.java
+++ app/src/main/java/com/example/se2einzelphase/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.se2einzelphase;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showPopup(View v) {
+        PopupMenu popupMenu = new PopupMenu(this, v);
+        popupMenu.inflate(R.menu.menu_items);
+        popupMenu.setOnMenuItemClickListener(this);
+        popupMenu.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        if (item.getItemId() == R.id.menu_item_calculation) {
+            Intent startCalculationIntent = new Intent(this, CalculationActivity.class);
+            startActivity(startCalculationIntent);
+            return true;
+        }
+        return false;
     }
 }
