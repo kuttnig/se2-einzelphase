5ebf94ba9e55a3f5f037c014606492fab0c4880a
Kevin Ernst Kuttnig <94113871+kuttnig@users.noreply.github.com>
2022-03-07 22:12:50 +0100

    Implement task 2 (calculation)

--- app/src/main/java/com/example/se2einzelphase/CalculationActivity.java
+++ app/src/main/java/com/example/se2einzelphase/CalculationActivity.java
@@ -7,13 +7,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CalculationActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
+    private EditText editTextUserInput;
+    private TextView textViewResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculation);
+
+        editTextUserInput = findViewById(R.id.activity_calculation_user_input);
+        textViewResult = findViewById(R.id.activity_calculation_result);
     }
 
     public void showPopup(View v) {
@@ -31,5 +38,43 @@ public class CalculationActivity extends AppCompatActivity implements PopupMenu.
             return true;
         }
         return false;
+    }
+
+    public void computeResult(View v) {
+        new Thread(() -> {
+            String userInput = editTextUserInput.getText().toString();
+            String s = "There is no pair with a GCD greater than 1.";
+
+            for (int i = 0; i < userInput.length(); i++) {
+                for (int j = (i + 1); j < userInput.length(); j++) {
+                    int n = Character.getNumericValue(userInput.charAt(i));
+                    int k = Character.getNumericValue(userInput.charAt(j));
+
+                    if (k > n) {
+                        int tmp = n;
+                        n = k;
+                        k = tmp;
+                    }
+                    while (k != 0) {
+                        int rem = n % k;
+                        n = k;
+                        k = rem;
+                    }
+
+                    if (n != 1) {
+                        s = "Pair found!\nDigit " + Character.getNumericValue(userInput.charAt(i)) + " at index " + i
+                                + "\nDigit " + Character.getNumericValue(userInput.charAt(j)) + " at index " + j
+                                + "\nGCD: " + n;
+
+                        i = userInput.length();
+                        j = userInput.length();
+                    }
+                }
+            }
+
+            // necessary because otherwise the ide complains: Variable used in lambda expression should be final or effectively final
+            String result = s;
+            textViewResult.post(() -> textViewResult.setText(result));
+        }).start();
     }
 }
