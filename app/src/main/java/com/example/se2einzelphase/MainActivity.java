4c183dd531f3e517198169f3f2e2b8905a10abaa
Kevin Ernst Kuttnig <94113871+kuttnig@users.noreply.github.com>
2022-03-07 19:21:49 +0100

    Implement task 1 (network test)
    This commit implements the first task with the help of the interface runnable. Fix some wrong references within the two layout files (activity_main.xml, activity_calculation.xml).

--- app/src/main/java/com/example/se2einzelphase/MainActivity.java
+++ app/src/main/java/com/example/se2einzelphase/MainActivity.java
@@ -5,16 +5,32 @@ import androidx.appcompat.widget.PopupMenu;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
+    private static final String TAG = "MainActivity";
+
+    private EditText editTextUserInput;
+    private TextView textViewServerResponse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextUserInput = findViewById(R.id.activity_main_user_input);
+        textViewServerResponse = findViewById(R.id.activity_main_server_response);
     }
 
     public void showPopup(View v) {
@@ -32,5 +48,26 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
             return true;
         }
         return false;
+    }
+
+    public void sendMessage(View v) {
+        new Thread(() -> {
+            try {
+                String userInput = editTextUserInput.getText().toString();
+
+                Socket clientSocket = new Socket("se2-isys.aau.at", 53212);
+                DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+                BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                outToServer.writeBytes(userInput + "\n");
+                String answerFromServer = inFromServer.readLine();
+                clientSocket.close();
+
+                textViewServerResponse.post(() -> textViewServerResponse.setText(answerFromServer));
+            } catch (UnknownHostException e) {
+                Log.e(TAG, "Hello, I'm a log-message.");
+            } catch (IOException e) {
+                Log.e(TAG, "It worked on my computer.");
+            }
+        }).start();
     }
 }
